eba47c1805a13cf4b743ff9a3ed4854916ebc5f4
Pawel Kondratowicz <pawel@pkondratowicz.onmicrosoft.com>
2025-02-17 09:20:31 +0100

    adding rbac role - fv secrets officer

--- k8s.lab.kv.tf
+++ k8s.lab.kv.tf
@@ -10,6 +10,11 @@ resource "azurerm_key_vault" "k8skv" {
   tenant_id                 = data.azurerm_client_config.current.tenant_id #tenant id here
   sku_name                  = "standard"
   enable_rbac_authorization = true
+}
 
+resource "azurerm_role_assignment" "k8skv_secrets_officer" {
+  scope                = azurerm_key_vault.k8skv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
